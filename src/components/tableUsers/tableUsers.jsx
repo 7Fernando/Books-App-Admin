414f3327816918cdc3d29bc7f2b674509f566413
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-05-05 01:36:36 -0300

    jose

--- src/components/tableUsers/tableUsers.jsx
+++ src/components/tableUsers/tableUsers.jsx
@@ -5,83 +5,76 @@ import { userColumns } from "../../datatablesource";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { deleteUser, getUsers, changeRol , getUserById} from "../../redux/actions/user";
-
-
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import {
+  deleteUser,
+  getUsers,
+  changeRol,
+  getUserById,
+} from "../../redux/actions/user";
 
 const Datatable = () => {
- 
-  const [open, setOpen] =useState(false)
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getUsers());
+    if (data.length === 0) {
+      dispatch(getUsers());
+    }
   }, [dispatch]);
   const allUser = useSelector((state) => state.user.allUser);
 
-const [id, setId] = useState (0)
-const [data, setData] = useState(allUser);
+  const [id, setId] = useState(0);
+  const [data, setData] = useState(allUser);
 
-//console.log('id', id)
   useEffect(() => {
-    dispatch(getUsers());
-    return (()=> 
-    setData(allUser)) 
+    return () => setData(allUser);
   }, [allUser]);
 
   const handleClickOpen = (id) => {
-    setId(id)
+    setId(id);
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
   };
 
-
   const handleDelete = (id) => {
-
     dispatch(deleteUser(id));
     setData(data.filter((item) => item.id !== id));
     setOpen(false);
   };
 
-  const handleSelect =(id)=>{
-    dispatch(changeRol(id))
-    dispatch(getUserById(id))
-  }
+  const handleSelect = (id) => {
+    dispatch(changeRol(id)).then((r) => dispatch(getUsers()));
+  };
 
-
-
-   
   const actionColumn = [
-
-  
     {
       field: "action",
       headerName: "Action",
       width: 300,
       renderCell: (params) => {
-      
         return (
-          
-
           <div className="cellAction">
-
             <div className="viewButton">
-
-       
-        <select labelId="demo-simple-select-label" id="demo-simple-select" onChange={(e)=>handleSelect(params.row.id)}>  
-      
-          <option value="selected" hidden> Change Rol</option>
-          <option value='ADMIN'>ADMIN</option>
-          <option value='USER'>USER</option>
-        </select>
-        </div> 
+              <select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                onChange={(e) => handleSelect(params.row.id)}
+              >
+                <option value="selected" hidden>
+                  {" "}
+                  Change Rol
+                </option>
+                <option value="ADMIN">ADMIN</option>
+                <option value="USER">USER</option>
+              </select>
+            </div>
 
             <Link
               to={`/users/${params.row.id}`}
@@ -93,34 +86,33 @@ const [data, setData] = useState(allUser);
            autoFocus >
             Delete 
         </Button> */}
-           
-      <Button  className="deleteButton" variant="outlined" onClick={()=> handleClickOpen(params.row.id)} >
-        Delete 
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          Delete User
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-          The record will be permanently deleted.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Back</Button>
-          <Button  onClick={() =>handleDelete(id)} 
-           autoFocus >
-               
-            Delete 
-          </Button>
-        </DialogActions>
-      </Dialog> 
-    
+
+            <Button
+              className="deleteButton"
+              variant="outlined"
+              onClick={() => handleClickOpen(params.row.id)}
+            >
+              Delete
+            </Button>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">Delete User</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  The record will be permanently deleted.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Back</Button>
+                <Button onClick={() => handleDelete(id)} autoFocus>
+                  Delete
+                </Button>
+              </DialogActions>
+            </Dialog>
           </div>
         );
       },
